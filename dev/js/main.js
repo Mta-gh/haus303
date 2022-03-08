f50f89099410570e1cc56d77ce2a8d282d4a38a9
unknown <mathias.greenhalgh@gmail.com>
2022-03-08 15:56:31 +0100

    logo js interval

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,10 +1,28 @@
-const { SassColor } = require("sass")
 
-const { SassColor } = require("sass")
 
-const { SassColor } = require("sass")
 
-const { SassColor } = require("sass")
 
-const { SassColor } = require("sass")
 
+let blob = document.querySelector(".hausBlob");
+let sguigg = document.querySelector(".hausSguigg");
+
+
+// setInterval(function(){
+//     blob.style.opacity = '1';
+    
+// }, 3000)
+
+
+
+(function() {     // function expression closure to contain variables
+    var i = 0;
+    var pics = [ "img/hausSquiggRender.png", "img/blobH.png" ];
+    var el = document.getElementById('logo-img');  // el doesn't change
+    console.log(el)
+
+    function toggle() {
+        el.src = pics[i];           // set the image
+        i = (i + 1) % pics.length;  // update the counter
+    }
+    setInterval(toggle, 300000);
+})();             // invoke the function expression
