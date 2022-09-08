e81307758aabb74abeafcc0ccc325df7a6ca2d53
Mat_Gh <mathias.greenhalgh@gmail.com>
2022-09-08 14:22:15 +0200

    bara js update

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,6 +1,6 @@
 // For logo change
 // let blob = document.querySelector(".hausBlob");
-let sguigg = document.querySelector(".hausSguigg");
+// let sguigg = document.querySelector(".hausSguigg");
 
 
 // setInterval(function(){
@@ -39,38 +39,113 @@ function mainContentAnim() {
 		// scale: 0,
 		ease: "back",
 	})
+};
+
+function helloHello() {
+	// let tl = gsap.timeline();
+	gsap.to(".main", {
+		duration: 0.8,
+		y: -2000,
+	});
+    console.log('hello hello')
+};
+
+
+function singleBookAnim() {
+    let tl = gsap.timeline();
+    tl.from(".book-pop__up", {
+        duration: 0.8,
+		opacity: 0,
+        scale: 1.2,
+        filter: "blur(45px)",
+        display:"none",
+        ease: "expo.out",
+    })
+};
+
+function singleBookAnimLeave() {
+    let tl = gsap.timeline();
+    tl.to(".book-pop__up", {
+        duration: 0.3,
+		opacity: 0,
+        scale: 1.2,
+        y: -200
+    })
 }
 
+function logoSize() {
+    gsap.to(".logo", {
+        duration: .3,
+        scale: .66,
+    })
+}
 
 
 function init() {
     let tlSideBar = gsap.timeline();
+    // tlSideBar.from(".marquee2", {
+    //     x: -130,
+    //     duration: .6,
+    // }).from(".marquee1", {
+    //     x: 130,
+    //     duration: .6,
+    // }, "<").from(".nav-left", {
+    //     x: -130,
+    //     duration: .6,
+    // }).from(".nav-right", {
+    //     x: 130,
+    //     duration: .6,
+    // }, "<");
+
+
     tlSideBar.from(".marquee2", {
         x: -130,
         duration: .6,
-    }).from(".marquee1", {
-        x: 130,
-        duration: .6,
-    }, "<").from(".nav-left", {
+    }).from(".nav-left", {
         x: -130,
         duration: .6,
-    }).from(".nav-right", {
-        x: 130,
-        duration: .6,
-    }, "<")
+    });
 
     barba.init({
         transitions: [{
-            name: 'default-transition',
-            leave() {
+            name: 'general-transition',
+            async leave(data) {
+                // return gsap.to(".main", {
+                //     duration: 0.6,
+                //     y: -500,
+                //     opacity: 0
+                // });
+                // helloHello()
             },
             enter(data) {
-                mainContentAnim()
+                // mainContentAnim();
+                // singleBookAnim()
+                logoSize()
             }
         }]
     });
 }
 init()
 
+
+// transitions: [{
+//     async leave(data) {
+//     window.scrollTo(0, 0);
+//     const done = this.async();
+//     contentAnimLeave();
+//     pageTransition();
+//     await delay(1000);
+//     done();
+    
+// },
+//  async enter(data) {
+//     // gsap.set(".overlay__transition", { opacity: 0 });
+//     window.scrollTo(0, 0);
+//     contentAnimEnter();
+//     titleSplit();
+// },
+// async once(data) {
+//     contentAnimEnter();
+//     titleSplit()
 
 
